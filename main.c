03009ca55cc8e60d81426f979e24522435d4ae95
demystik <demystiktech@gmail.com>
2022-09-08 17:45:19 +0100

    betty in main updated

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 /**
  * main - monty execution starts
  * Author - Thaoban Abdrasheed
+ * @lineptr: pointer to a buffer
+ * @n: number of size to read
+ * @stream: The file stream to read from
  *
  * Return: 0 on success
  */
@@ -20,17 +23,15 @@ int main(int argc, char *argv[])
 	int num = 0;
 	stack_t *buff = NULL;
 
-
-	
 	if (argc != 2)
 	{
-	    fprintf(stderr,"USAGE: monty file\n");
-    	    exit(EXIT_FAILURE);
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
 	file_ptr = fopen(argv[1], "r");
 	if (file_ptr == NULL)
 	{
-		fprintf(stderr,"Error: Can't open file %s",argv[1]);
+		fprintf(stderr, "Error: Can't open file %s", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((line_read = getline(&buffer, &count, file_ptr)) != -1)
@@ -42,27 +43,27 @@ int main(int argc, char *argv[])
 		arg = strtok(NULL, " ");
 
 		if (upcode == NULL)
-			continue;	
+			continue;
 
 		if (strcmp(upcode, "push") == 0)
-	        {
+		{
 			if (if_num(arg) == 0)
 			{
-				fprintf(stderr,"L%d: usage: push integer", line_num);
+				fprintf(stderr, "L%d: usage: push integer", line_num);
 				exit(EXIT_FAILURE);
 			}
 			num = atoi(arg);
-                	fun_push(&buff, num);
-	        }
+			fun_push(&buff, num);
+		}
 
 		if (strcmp(upcode, "pall") == 0)
 			fun_pall(&buff, line_num);
 
-	        else if (strcmp(upcode, "pint") == 0)
-               		fun_pint(&buff, line_num);
+		else if (strcmp(upcode, "pint") == 0)
+			fun_pint(&buff, line_num);
 
-	        else if (strcmp(upcode, "pop") == 0)
-	        	fun_pop(&buff, line_num);
+		else if (strcmp(upcode, "pop") == 0)
+			fun_pop(&buff, line_num);
 	}
 
 	fclose(file_ptr);
